4c03133810322f33e1e910cca2bf462b08d136d7
Cameron Smith <114649266+smathles@users.noreply.github.com>
2024-11-24 18:18:21 +1000

    fix lint.lua

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -47,16 +47,9 @@ return {
       vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
         group = lint_augroup,
         callback = function()
--- <<<<<<< HEAD
-          -- Only run the linter in buffers that you can modify in order to
-          -- avoid superfluous noise, notably within the handy LSP pop-ups that
-          -- describe the hovered symbol using Markdown.
-          if vim.opt_local.modifiable:get() then
--- =======
           -- Check if the buffer is modifiable and the file exists
           local filename = vim.fn.expand '%:p' -- Get the absolute file path
           if vim.opt_local.modifiable:get() and vim.fn.filereadable(filename) == 1 then
--- >>>>>>> af252ce (setup new git repo)
             lint.try_lint()
           end
         end,
