0ad02d54b32be952db27771211e697600be12c56
Cameron Smith <114649266+smathles@users.noreply.github.com>
2025-01-16 23:45:14 +1000

    Add c/json/md LSPs, clean up comments

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -51,7 +51,6 @@ return { -- Autocompletion
       -- For an understanding of why these mappings were
       -- chosen, you will need to read `:help ins-completion`
       --
-      -- No, but seriously. Please read `:help ins-completion`, it is really good!
       mapping = cmp.mapping.preset.insert {
         -- Select the [n]ext item
         ['<C-n>'] = cmp.mapping.select_next_item(),
@@ -63,12 +62,9 @@ return { -- Autocompletion
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
 
         -- Accept ([y]es) the completion.
-        --  This will auto-import if your LSP supports it.
-        --  This will expand snippets if the LSP sent a snippet.
         ['<C-y>'] = cmp.mapping.confirm { select = true },
 
-        -- If you prefer more traditional completion keymaps,
-        -- you can uncomment the following lines
+        -- More traditional completion keymaps,
         ['<CR>'] = cmp.mapping.confirm { select = true },
         ['<Tab>'] = cmp.mapping.select_next_item(),
         ['<S-Tab>'] = cmp.mapping.select_prev_item(),
