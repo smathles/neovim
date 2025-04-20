29c04926c1e967d4a2e78c85eb12afaeb4ffea2f
Cameron Smith <114649266+smathles@users.noreply.github.com>
2025-04-20 23:23:16 +1000

    add formatting patch

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -1,4 +1,4 @@
-re { -- Autoformat
+return { -- Autoformat
   'stevearc/conform.nvim',
   event = { 'BufWritePre' },
   cmd = { 'ConformInfo' },
@@ -18,17 +18,18 @@ re { -- Autoformat
       -- Disable "format_on_save lsp_fallback" for languages that don't
       -- have a well standardized coding style. You can add additional
       -- languages here or re-enable it for the disabled ones.
-      local disable_filetypes = { c = true, cpp = true }
-      local lsp_format_opt
-      if disable_filetypes[vim.bo[bufnr].filetype] then
-        lsp_format_opt = 'never'
-      else
-        lsp_format_opt = 'fallback'
-      end
-      return {
-        timeout_ms = 500,
-        lsp_format = lsp_format_opt,
+      local disable_filetypes = {
+        c = true,
+        cpp = true,
       }
+      if disable_filetypes[vim.bo[bufnr].filetype] then
+        return nil
+      else
+        return {
+          timeout_ms = 500,
+          lsp_format = 'fallback',
+        }
+      end
     end,
     formatters_by_ft = {
       lua = { 'stylua' },
