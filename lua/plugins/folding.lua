64d06ff299023d2fca98e0c0cf0b159ee2e46276
Cameron Smith <cameronsmith002@gmail.com>
2025-08-05 20:21:52 +1000

    emergency backup for patching things

--- lua/plugins/folding.lua
+++ lua/plugins/folding.lua
@@ -3,52 +3,52 @@
 -- To be honest, there's SO much more this plugin can do! Like a "folding" column,
 -- preview of the folded content etc. This is a very minimal config!
 return { -- lazy.nvim plugin specification
-  {
-    'kevinhwang91/nvim-ufo',
-    dependencies = { 'kevinhwang91/promise-async' },
-    config = function()
-      -- General fold settings
-      vim.o.foldcolumn = '0'
-      vim.o.foldlevel = 99
-      vim.o.foldlevelstart = 99
-      vim.o.foldenable = true
-
-      -- Key mappings for ufo
-      vim.keymap.set('n', 'zR', require('ufo').openAllFolds, { desc = 'Open all folds' })
-      vim.keymap.set('n', 'zM', require('ufo').closeAllFolds, { desc = 'Close all folds' })
-
-      -- Setup ufo with options (choose one below)
-      require('ufo').setup {
-        -- Example for using treesitter and indent
-        provider_selector = function(_, _, _)
-          return { 'treesitter', 'indent' }
-        end,
-      }
-    end,
-  },
-
-  { -- Optional LSP configuration for nvim-ufo
-    'neovim/nvim-lspconfig',
-    config = function()
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities.textDocument.foldingRange = {
-        dynamicRegistration = false,
-        lineFoldingOnly = true,
-      }
-
-      local lspconfig = require 'lspconfig'
-      local servers = lspconfig.util.available_servers() -- List of all available servers
-      for _, server in ipairs(servers) do
-        lspconfig[server].setup {
-          capabilities = capabilities,
-        }
-      end
-    end,
-  },
-  -- Optional: coc.nvim setup
   -- {
-  --   'neoclide/coc.nvim',
-  --   branch = 'master',
-  --   build = 'yarn install --frozen-lockfile',
+  --   'kevinhwang91/nvim-ufo',
+  --   dependencies = { 'kevinhwang91/promise-async' },
+  --   config = function()
+  --     -- General fold settings
+  --     vim.o.foldcolumn = '0'
+  --     vim.o.foldlevel = 99
+  --     vim.o.foldlevelstart = 99
+  --     vim.o.foldenable = true
+  --
+  --     -- Key mappings for ufo
+  --     vim.keymap.set('n', 'zR', require('ufo').openAllFolds, { desc = 'Open all folds' })
+  --     vim.keymap.set('n', 'zM', require('ufo').closeAllFolds, { desc = 'Close all folds' })
+  --
+  --     -- Setup ufo with options (choose one below)
+  --     require('ufo').setup {
+  --       -- Example for using treesitter and indent
+  --       provider_selector = function(_, _, _)
+  --         return { 'treesitter', 'indent' }
+  --       end,
+  --     }
+  --   end,
   -- },
+  --
+  -- { -- Optional LSP configuration for nvim-ufo
+  --   'neovim/nvim-lspconfig',
+  --   config = function()
+  --     local capabilities = vim.lsp.protocol.make_client_capabilities()
+  --     capabilities.textDocument.foldingRange = {
+  --       dynamicRegistration = false,
+  --       lineFoldingOnly = true,
+  --     }
+  --
+  --     local lspconfig = require 'lspconfig'
+  --     local servers = vim.lsp.config.util.available_servers() -- List of all available servers
+  --     for _, server in ipairs(servers) do
+  --       lspconfig[server].setup {
+  --         capabilities = capabilities,
+  --       }
+  --     end
+  --   end,
+  -- },
+  -- -- Optional: coc.nvim setup
+  -- -- {
+  -- --   'neoclide/coc.nvim',
+  -- --   branch = 'master',
+  -- --   build = 'yarn install --frozen-lockfile',
+  -- -- },
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,43 +1,107 @@
-return { -- Highlight, edit, and navigate code
+return {
   'nvim-treesitter/nvim-treesitter',
+  lazy = false,
   build = ':TSUpdate',
-  main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-  opts = {
-    ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'hyprlang' },
-    -- Autoinstall languages that are not installed
-    auto_install = true,
-    highlight = {
-      enable = true,
-      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-      --  If you are experiencing weird indenting issues, add the language to
-      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { 'ruby' },
-    },
-    indent = { enable = true, disable = { 'ruby' } },
-    fold = {
-      enable = true,
-      disable = 'ruby',
-    },
-    -- Ensure comments are recognized for folding
-    fold_extra = {
-      enable = true,
-      disable = 'ruby',
-    },
-  },
-  config = function(_, opts)
-    -- Add filetype detection for custom patterns
-    vim.filetype.add {
-      extension = { rasi = 'rasi' },
-      pattern = {
-        ['.*/kitty/*.conf'] = 'bash',
-        ['.*/hypr/.*%.conf'] = 'hyprlang', -- Detect Hyprland config files
+  config = function()
+    require('nvim-treesitter.configs').setup {
+      ensure_installed = {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'vim',
+        'vimdoc',
+      },
+      auto_install = true,
+      highlight = {
+        -- enable = true,
+        enable = false,
+        -- You can remove this line entirely if not using regex-based fallback highlighting
+        additional_vim_regex_highlighting = false,
+      },
+      indent = {
+        enable = true,
       },
     }
-    require('nvim-treesitter.configs').setup(opts)
+
+    -- Custom filetype detection
+    vim.filetype.add {
+      extension = {
+        rasi = 'rasi',
+      },
+      pattern = {
+        ['.*/kitty/.*%.conf'] = 'bash',
+        ['.*/hypr/.*%.conf'] = 'hyprlang',
+      },
+    }
   end,
-  -- There are additional nvim-treesitter modules that you can use to interact
-  -- with nvim-treesitter. You should go explore a few and see what interests you:
-  --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-  --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-  --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 }
+
+-- return { -- Highlight, edit, and navigate code
+--   'nvim-treesitter/nvim-treesitter',
+--   lazy = false,
+--   branch = 'main',
+--   build = ':TSUpdate',
+--   main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+--   -- [[ Configure Treesitter ]] See `:help nvim-treesitter-intro`
+--   config = function()
+--     local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
+--     require('nvim-treesitter').install(parsers)
+--
+--     vim.api.nvim_create_autocmd('FileType', {
+--       pattern = parsers,
+--       callback = function()
+--         -- enbales syntax highlighting and other treesitter features
+--         vim.treesitter.start()
+--
+--         -- enbales treesitter based folds
+--         -- for more info on folds see `:help folds`
+--         -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+--
+--         -- enables treesitter based indentation
+--         vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+--       end,
+--     })
+--     -- Add filetype detection for custom patterns
+--     vim.filetype.add {
+--       extension = { rasi = 'rasi' },
+--       pattern = {
+--         ['.*/kitty/*.conf'] = 'bash',
+--         ['.*/hypr/.*%.conf'] = 'hyprlang', -- Detect Hyprland config files
+--       },
+--     }
+--   end,
+
+-- opts = {
+--   ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'hyprlang' },
+--   -- Autoinstall languages that are not installed
+--   auto_install = true,
+--   highlight = {
+--     enable = true,
+--     -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+--     --  If you are experiencing weird indenting issues, add the language to
+--     --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+--     additional_vim_regex_highlighting = { 'ruby' },
+--   },
+--   indent = { enable = true, disable = { 'ruby' } },
+--   fold = {
+--     enable = true,
+--     disable = 'ruby',
+--   },
+--   -- Ensure comments are recognized for folding
+--   fold_extra = {
+--     enable = true,
+--     disable = 'ruby',
+--   },
+-- },
+-- There are additional nvim-treesitter modules that you can use to interact
+-- with nvim-treesitter. You should go explore a few and see what interests you:
+--    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+--    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+-- }
