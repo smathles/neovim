4fd90e55d858f06c212f63796dfc5e6376ca207a
Cameron Smith <cameronsmith002@gmail.com>
2025-08-21 18:22:40 +1000

    I forgor. LSP debugging, [was an upstream bug in nvim]

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,8 @@
 return { -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+  -- branch = 'main',
   build = ':TSUpdate',
-  main = 'nvim-treesitter.configs', -- Sets main module to use for opts
   opts = {
     ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'hyprlang' },
     -- Autoinstall languages that are not installed
@@ -13,12 +14,14 @@ return { -- Highlight, edit, and navigate code
       --  the list of additional_vim_regex_highlighting and disabled languages for indent.
       additional_vim_regex_highlighting = { 'ruby' },
     },
-    indent = { enable = true, disable = { 'ruby' } },
+    indent = {
+      enable = true,
+      disable = 'ruby',
+    },
     fold = {
       enable = true,
       disable = 'ruby',
     },
-    -- Ensure comments are recognized for folding
     fold_extra = {
       enable = true,
       disable = 'ruby',
@@ -33,11 +36,18 @@ return { -- Highlight, edit, and navigate code
         ['.*/hypr/.*%.conf'] = 'hyprlang', -- Detect Hyprland config files
       },
     }
-    require('nvim-treesitter.configs').setup(opts)
+    require('nvim-treesitter').setup(opts)
   end,
   -- There are additional nvim-treesitter modules that you can use to interact
   -- with nvim-treesitter. You should go explore a few and see what interests you:
   --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
   --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
   --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+
+  -- NOTE: In the future, the master treesitter branch will be deprecated and modules will no longer exist. Keep the above config for now.
+
+  -- 'nvim-treesitter/nvim-treesitter',
+  -- lazy = false,
+  -- branch = 'main',
+  -- build = ':TSUpdate',
 }
