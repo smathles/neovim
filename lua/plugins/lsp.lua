1cabd46b63518ba702fbf5868fafd27576ad6d77
Cameron Smith <cameronsmith002@gmail.com>
2025-12-13 16:43:28 +1000

    update gr* g* notation

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -54,40 +54,40 @@ return { -- LSP Plugins
 
           -- Rename the variable under your cursor.
           --  Most Language Servers support renaming across files, etc.
-          map('grn', vim.lsp.buf.rename, '[R]e[n]ame')
+          map('grn', vim.lsp.buf.rename, 'Re[n]ame')
 
           -- Execute a code action, usually your cursor needs to be on top of an error
           -- or a suggestion from your LSP for this to activate.
-          map('gra', vim.lsp.buf.code_action, '[G]oto Code [a]ction', { 'n', 'x' })
+          map('gra', vim.lsp.buf.code_action, 'Code [a]ctions', { 'n', 'x' })
 
           -- Find references for the word under your cursor.
-          map('grr', require('telescope.builtin').lsp_references, '[G]oto [r]eferences')
+          map('grr', require('telescope.builtin').lsp_references, 'Go to [r]eferences')
 
           -- Jump to the implementation of the word under your cursor.
           --  Useful when your language has ways of declaring types without an actual implementation.
-          map('grI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+          map('grI', require('telescope.builtin').lsp_implementations, 'Go to [I]mplementation')
 
           -- Jump to the definition of the word under your cursor.
           --  This is where a variable was first declared, or where a function is defined, etc.
           --  To jump back, press <C-t>.
-          map('grd', require('telescope.builtin').lsp_definitions, '[G]oto [d]efinition')
+          map('grd', require('telescope.builtin').lsp_definitions, 'Go to [d]efinition')
 
-          -- WARN: This is not Goto Definition, this is Goto Declaration.
+          -- WARN: This is not "Go to Definition", this is "Go to Declaration".
           --  For example, in C this would take you to the header.
-          map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+          map('grD', vim.lsp.buf.declaration, 'Go to [D]eclaration')
 
           -- Fuzzy find all the symbols in your current document.
           --  Symbols are things like variables, functions, types, etc.
-          map('gs', require('telescope.builtin').lsp_document_symbols, 'Open Document [s]ymbols')
+          map('grs', require('telescope.builtin').lsp_document_symbols, 'Open Document [s]ymbols')
 
           -- Fuzzy find all the symbols in your current workspace.
           --  Similar to document symbols, except searches over your entire project.
-          map('gW', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Open [W]orkspace Symbols')
+          map('grW', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Open [W]orkspace Symbols')
 
           -- Jump to the type of the word under your cursor.
           --  Useful when you're not sure what type a variable is and you want to see
           --  the definition of its *type*, not where it was *defined*.
-          map('grt', require('telescope.builtin').lsp_type_definitions, '[G]oto [t]ype Definition')
+          map('grt', require('telescope.builtin').lsp_type_definitions, 'Go to [t]ype Definition')
 
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
@@ -186,7 +186,7 @@ return { -- LSP Plugins
         rust_analyzer = {},
         marksman = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
@@ -228,7 +228,7 @@ return { -- LSP Plugins
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
-        'hyprls', -- Hyprlang support
+        -- 'hyprls', -- Hyprlang support
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
